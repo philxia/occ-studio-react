737288e10c59487ae11d12c5eb7bcb7c53317c47
Phil Xia <phil.xia@gmail.com>
2023-01-28 15:02:07 -0500

    view ports

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,249 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import {
+  CommandBar,
+  ICommandBarItemProps,
+} from "@fluentui/react/lib/CommandBar";
+import { IButtonProps } from "@fluentui/react/lib/Button";
+import { setVirtualParent } from "@fluentui/dom-utilities";
+import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
 
+const overflowProps: IButtonProps = { ariaLabel: "More commands" };
+
+export const CommandBarBasicExample: React.FunctionComponent = () => {
+  return (
+    <CommandBar
+      items={_items}
+      overflowItems={_overflowItems}
+      overflowButtonProps={overflowProps}
+      farItems={_farItems}
+      ariaLabel="Inbox actions"
+      primaryGroupAriaLabel="Email actions"
+      farItemsGroupAriaLabel="More actions"
+    />
+  );
+};
+
+const _items: ICommandBarItemProps[] = [
+  {
+    key: "newItem",
+    text: "New",
+    cacheKey: "myCacheKey", // changing this key will invalidate this item's cache
+    iconProps: { iconName: "Add" },
+    subMenuProps: {
+      items: [
+        {
+          key: "emailMessage",
+          text: "Email message",
+          iconProps: { iconName: "Mail" },
+          ["data-automation-id"]: "newEmailButton", // optional
+        },
+        {
+          key: "calendarEvent",
+          text: "Calendar event",
+          iconProps: { iconName: "Calendar" },
+        },
+      ],
+    },
+  },
+  {
+    key: "upload",
+    text: "Upload",
+    iconProps: { iconName: "Upload" },
+    subMenuProps: {
+      items: [
+        {
+          key: "uploadfile",
+          text: "File",
+          preferMenuTargetAsEventTarget: true,
+          onClick: (
+            ev?:
+              | React.MouseEvent<HTMLElement, MouseEvent>
+              | React.KeyboardEvent<HTMLElement>
+              | undefined
+          ) => {
+            ev?.persist();
+
+            Promise.resolve().then(() => {
+              const inputElement = document.createElement("input");
+              inputElement.style.visibility = "hidden";
+              inputElement.setAttribute("type", "file");
+
+              document.body.appendChild(inputElement);
+
+              const target = ev?.target as HTMLElement | undefined;
+
+              if (target) {
+                setVirtualParent(inputElement, target);
+              }
+
+              inputElement.click();
+
+              if (target) {
+                setVirtualParent(inputElement, null);
+              }
+
+              setTimeout(() => {
+                inputElement.remove();
+              }, 10000);
+            });
+          },
+        },
+        {
+          key: "uploadfolder",
+          text: "Folder",
+          preferMenuTargetAsEventTarget: true,
+          onClick: (
+            ev?:
+              | React.MouseEvent<HTMLElement, MouseEvent>
+              | React.KeyboardEvent<HTMLElement>
+              | undefined
+          ) => {
+            ev?.persist();
+
+            Promise.resolve().then(() => {
+              const inputElement = document.createElement("input");
+              inputElement.style.visibility = "hidden";
+              inputElement.setAttribute("type", "file");
+
+              (inputElement as { webkitdirectory?: boolean }).webkitdirectory =
+                true;
+
+              document.body.appendChild(inputElement);
+
+              const target = ev?.target as HTMLElement | undefined;
+
+              if (target) {
+                setVirtualParent(inputElement, target);
+              }
+
+              inputElement.click();
+
+              if (target) {
+                setVirtualParent(inputElement, null);
+              }
+
+              setTimeout(() => {
+                inputElement.remove();
+              }, 10000);
+            });
+          },
+        },
+      ],
+    },
+  },
+  {
+    key: "share",
+    text: "Share",
+    iconProps: { iconName: "Share" },
+    onClick: () => console.log("Share"),
+  },
+  {
+    key: "download",
+    text: "Download",
+    iconProps: { iconName: "Download" },
+    onClick: () => console.log("Download"),
+  },
+];
+
+const _overflowItems: ICommandBarItemProps[] = [
+  {
+    key: "move",
+    text: "Move to...",
+    onClick: () => console.log("Move to"),
+    iconProps: { iconName: "MoveToFolder" },
+  },
+  {
+    key: "copy",
+    text: "Copy to...",
+    onClick: () => console.log("Copy to"),
+    iconProps: { iconName: "Copy" },
+  },
+  {
+    key: "rename",
+    text: "Rename...",
+    onClick: () => console.log("Rename"),
+    iconProps: { iconName: "Edit" },
+  },
+];
+
+const _farItems: ICommandBarItemProps[] = [
+  {
+    key: "tile",
+    text: "Grid view",
+    // This needs an ariaLabel since it's icon-only
+    ariaLabel: "Grid view",
+    iconOnly: true,
+    iconProps: { iconName: "Tiles" },
+    onClick: () => console.log("Tiles"),
+  },
+  {
+    key: "info",
+    text: "Info",
+    // This needs an ariaLabel since it's icon-only
+    ariaLabel: "Info",
+    iconOnly: true,
+    iconProps: { iconName: "Info" },
+    onClick: () => console.log("Info"),
+  },
+];
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
 export function App() {
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor: null, monaco: any) {
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    alert(editorRef.current?.getValue());
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CommandBarBasicExample />
+      <div className="flex-container">
+        <div className="flex-child magenta">
+          <Editor
+            height="90vh"
+            defaultLanguage="javascript"
+            defaultValue="// some comment"
+            onMount={handleEditorDidMount}
+          />{" "}
+        </div>
+        <div className="flex-child green">
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+          </Canvas>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { App } from "./App";
 import "./index.css";
+import { createRoot } from 'react-dom/client';
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// initialize icons service.
+initializeIcons();
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
